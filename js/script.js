c38d53d60d37a5af1b06714c5d1129495174f460
kavalar <apuc06@mail.ru>
2018-03-14 13:38:10 +0300

    add feedbacks

--- js/script.js
+++ js/script.js
@@ -1,21 +1,68 @@
-var message="Правый клик запрещен!";
-function clickIE4(){
-      if (event.button==2){
-            return false;
-      }
+// var message="Правый клик запрещен!";
+// function clickIE4(){
+//       if (event.button==2){
+//             return false;
+//       }
+// }
+// function clickNS4(e){
+//       if (document.layers||document.getElementById&&!document.all){
+//             if (e.which==2||e.which==3){
+//                   return false;
+//             }
+//       }
+// }
+// if (document.layers){
+//       document.captureEvents(Event.MOUSEDOWN);
+//       document.onmousedown=clickNS4;
+// }
+// else if (document.all&&!document.getElementById){
+//       document.onmousedown=clickIE4;
+// }
+// document.oncontextmenu=new Function("return false")
+
+/*--  Запрет правого клика по картинкам  --*/
+var imgs = $('img');
+
+if (imgs.length > 0) {
+    imgs.each(function () {
+        $(this)[0].oncontextmenu = function () {
+            return false
+        }
+    })
 }
-function clickNS4(e){
-      if (document.layers||document.getElementById&&!document.all){
-            if (e.which==2||e.which==3){
-                  return false;
+
+/*--  Слайдер на странице отзывов  --*/
+$('.delivery__slider').slick({
+    centerMode: true,
+    centerPadding: '110px',
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
             }
-      }
-}
-if (document.layers){
-      document.captureEvents(Event.MOUSEDOWN);
-      document.onmousedown=clickNS4;
-}
-else if (document.all&&!document.getElementById){
-      document.onmousedown=clickIE4;
-}
-document.oncontextmenu=new Function("return false")
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '120px',
+                slidesToShow: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '60px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
